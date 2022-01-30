e7f3010864e87a23a22f47192b404ba1f5cddf4f
Bob F <bobfdev@gmail.com>
2022-01-29 18:16:20 -0600

    completed files

--- app.js
+++ app.js
@@ -6,21 +6,29 @@ const selectedIndex = null;
 
 imageIndexes.forEach((i) => {
     const image = document.createElement('img');
-    image.src = `/images/city-${i}.jpg`;
+    // image.src = `/images/compressed/city-${i}.jpg`;
+    // image.src = `https://ik.imagekit.io/bbnxglxuzxq/city-${i}.jpg`;
+    // transform image size to free up space and improve performance (tr:w-600:h-600)
+    image.src = `https://ik.imagekit.io/bbnxglxuzxq/tr:w-600:h-600/city-${i}.jpg`;
     image.alt = `Southern City ${i}`;
+    gallery.appendChild(image);
     image.classList.add('galleryImg');
 
     image.addEventListener('click', () => {
         popup.style.transform = `translateY(0)`;
-        selectedImage.src = `/images/city-${i}.jpg`;
+        // selectedImage.src = `/images/city-${i}.jpg`;
+        // transform image size to free up space and improve performance (tr:w-600:h-600)
+        selectedImage.src = `https://ik.imagekit.io/bbnxglxuzxq/tr:w-800:h-800/city-${i}.jpg`;
+        selectedImage.srcset = `https://ik.imagekit.io/bbnxglxuzxq/tr:w-400/city-${i}.jpg 400w,
+                                https://ik.imagekit.io/bbnxglxuzxq/tr:w-800/city-${i}.jpg 800w,
+                                https://ik.imagekit.io/bbnxglxuzxq/tr:w-1200/city-${i}.jpg 1200w`;
         selectedImage.alt = `Southern City ${i}`;
-    })
-
-    gallery.appendChild(image);
-})
+    });
+});
 
 popup.addEventListener('click', () => {
     popup.style.transform = `translateY(-100%)`;
-    popup.src = '';
-    popup.alt = '';
+    selectedImage.src = '';
+    selectedImage.srcset = '';
+    selectedImage.alt = '';
 })
